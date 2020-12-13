356bd48ecbfc3b79a623f4f49b65c1f195524925
measmeng <measraim@usc.edu>
2020-12-13 01:24:36 -0800

    fix download url for archive files

--- scripts/download-eia-data.sh
+++ scripts/download-eia-data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd '/Users/MEAS/Google Drive/data/eia/weekly-coal-production'
-for i in $(seq -w 1984 2020)
+
+for i in $(seq -w 2015 2020)
     do wget -nc 'https://www.eia.gov/coal/production/weekly/current_year/weekprod'$i'tot.xls'
+    done
+    
+for i in $(seq -w 1984 2014)
+    do wget -nc 'https://www.eia.gov/coal/production/weekly/archive/weekprod'$i'tot.xls'
     done
