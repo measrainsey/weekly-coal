473807f00af5cc46eba5a0700989fbe97c0aec76
measmeng <measraim@usc.edu>
2020-12-13 15:56:29 -0800

    download 2020 data separately

--- scripts/download-eia-data.sh
+++ scripts/download-eia-data.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cd '/Users/MEAS/Google Drive/data/eia/weekly-coal-production'
 
-for i in $(seq -w 2015 2020)
-    do wget -nc 'https://www.eia.gov/coal/production/weekly/current_year/weekprod'$i'tot.xls'
-    done
-    
-for i in $(seq -w 1984 2014)
-    do wget -nc 'https://www.eia.gov/coal/production/weekly/archive/weekprod'$i'tot.xls'
-    done
+# for 2020, get original estimates:
+    wget -nc 'https://www.eia.gov/coal/production/weekly/current_year/weekprodforecast2020tot.xls'
+
+# for all historic years, get revised estimates:
+    for i in $(seq -w 2015 2019)
+        do wget -nc 'https://www.eia.gov/coal/production/weekly/current_year/weekprod'$i'tot.xls'
+        done
+
+    for i in $(seq -w 1984 2014)
+        do wget -nc 'https://www.eia.gov/coal/production/weekly/archive/weekprod'$i'tot.xls'
+        done
